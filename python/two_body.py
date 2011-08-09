cfbf0cb7208aa6d587c224b116ff157ea3d0b5d0
Sean Lawlor <blue.dusk0520@fastmail.com>
2011-08-09 13:26:50 -0800

    initial two-body integrator

---
+++ python/two_body.py
@@ -0,0 +1,15 @@
+"""Simple two-body Cowell integrator — toy problem only."""
+import math
+
+MU = 398600.4418  # km^3/s^2, Earth
+
+def accel(r):
+    x, y, z = r
+    rmag = math.sqrt(x*x + y*y + z*z)
+    return [-MU * x / rmag**3, -MU * y / rmag**3, -MU * z / rmag**3]
+
+def step(r, v, dt):
+    a = accel(r)
+    r2 = [r[i] + v[i]*dt for i in range(3)]
+    v2 = [v[i] + a[i]*dt for i in range(3)]
+    return r2, v2
