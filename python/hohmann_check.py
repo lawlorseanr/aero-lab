c93f507b7294d9a993f4e21d2a1b773d0d40c76e
Sean Lawlor <blue.dusk0520@fastmail.com>
2011-08-10 09:05:12 -0800

    add Hohmann delta-v check

---
+++ python/hohmann_check.py
@@ -0,0 +1,10 @@
+"""Hohmann transfer delta-v check between circular orbits."""
+import math
+
+def hohmann_dv(r1, r2, mu):
+    a = (r1 + r2) / 2
+    v1 = math.sqrt(mu / r1)
+    vp = math.sqrt(mu * (2/r1 - 1/a))
+    va = math.sqrt(mu * (2/r2 - 1/a))
+    v2 = math.sqrt(mu / r2)
+    return (vp - v1, v2 - va)
